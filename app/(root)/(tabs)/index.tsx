4ed8c506694e9f850d9c4643e47f26699cccf471
Kaustubh Patil <patilkaustubh990@gmail.com>
2025-02-15 20:32:16 +0530

    Implement sign-in, explore, and profile screens; update app scheme and remove unused components (define routes)

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import SignIn from "../../sign-in";
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href=".././sign-in">SignIn</Link>
+      <Link href="./Explore">Explore</Link>
+      <Link href="./Profile">Profile</Link>
+      <Link href="./properties/1">Property</Link>
+    </View>
+  );
+}
