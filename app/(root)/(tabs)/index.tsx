e288735755d1835abe558c14671cc44e2d22315d
Kaustubh Patil <patilkaustubh990@gmail.com>
2025-02-20 00:32:42 +0530

    Refactor welcome message styling and remove unused image assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,12 +5,12 @@ export default function Index() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        // flex: 1,
+        // justifyContent: "center",
+        // alignItems: "center",
       }}
     >
-      <Text className="text-gray-700 text-3xl font-bold">Welcome to UrbanKeys</Text>
+      <Text className="text-gray-700 text-3xl font-bold justify-center items-center px-11 py-3">Welcome to UrbanKeys</Text>
       <Link href=".././sign-in">SignIn</Link>
       <Link href="./Explore">Explore</Link>
       <Link href="./Profile">Profile</Link>
