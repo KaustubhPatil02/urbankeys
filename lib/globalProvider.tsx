64f275cc22ee897860035433830b1fff54f6a591
Kaustubh Patil <patilkaustubh990@gmail.com>
2025-02-26 06:43:53 +0530

    Rename getUser to getCurrentUser and implement GlobalProvider with user state management and refetch functionality

--- lib/globalProvider.tsx
+++ lib/globalProvider.tsx
@@ -1,5 +1,6 @@
-import { createContext } from "react";
-
+import { createContext, ReactNode, useContext } from "react";
+import { useAppwriteHook } from "./useAppwriteHook";
+import { getCurrentUser } from "./appwrite";
 interface User {
     $id: string;
     name: string;
@@ -10,9 +11,41 @@ interface GlobalContextType {
     isLoggedIn: boolean;
     user: User | null;
     loading: boolean;
+    refetch: (newParams?: Record<string, string | number>) => Promise<void>;
 }
 
 
 
 const GlobalContext = createContext<GlobalContextType | undefined>(undefined)
-export default GlobalContext
+    export const GlobalProvider = ({ children }: { children: ReactNode }) => {
+
+    const {
+        data: user,
+        loading,
+        refetch
+    } = useAppwriteHook({
+        fn : getCurrentUser
+    });
+    const isLoggedIn = !!user;
+    console.log(JSON.stringify(user, null, 2))
+    return(
+        <GlobalContext.Provider value={{
+            isLoggedIn,
+            user,
+            loading,
+            refetch
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () : GlobalContextType => {
+    const context = useContext(GlobalContext)
+    if (!context) {
+        throw new Error('useGlobalContext must be used within a GlobalProvider')
+    }
+    return context
+}
+
+export default GlobalProvider;
